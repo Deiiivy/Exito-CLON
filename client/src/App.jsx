d62a489ad947db6cc75995c0bbb9b5d5a022269c
Deiiivy <deivypr28@gmail.com>
2024-02-26 19:32:01 -0500

    initializing project

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+   <div>exito</div>
+  )
+}
+
+export default App
